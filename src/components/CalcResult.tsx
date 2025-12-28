c788a7d4f570161d7aa4256585eee2d47347cb6b
hyunwoo0081 <hyunwoo0081@gmail.com>
2025-12-28 22:36:23 +0900

    feat: 컴포넌트 분리

---
+++ src/components/CalcResult.tsx
@@ -0,0 +1,46 @@
+import { colors, ListRow } from 'tosslib';
+
+function CalcResult() {
+  return (
+    <>
+      <ListRow
+        contents={
+          <ListRow.Texts
+            type="2RowTypeA"
+            top="예상 수익 금액"
+            topProps={{ color: colors.grey600 }}
+            bottom={`1,000,000원`}
+            bottomProps={{ fontWeight: 'bold', color: colors.blue600 }}
+          />
+        }
+      />
+      <ListRow
+        contents={
+          <ListRow.Texts
+            type="2RowTypeA"
+            top="목표 금액과의 차이"
+            topProps={{ color: colors.grey600 }}
+            bottom={`-500,000원`}
+            bottomProps={{ fontWeight: 'bold', color: colors.blue600 }}
+          />
+        }
+      />
+      <ListRow
+        contents={
+          <ListRow.Texts
+            type="2RowTypeA"
+            top="추천 월 납입 금액"
+            topProps={{ color: colors.grey600 }}
+            bottom={`100,000원`}
+            bottomProps={{ fontWeight: 'bold', color: colors.blue600 }}
+          />
+        }
+      />
+
+      {/* 아래는 사용자가 적금 상품을 선택하지 않고 계산 결과 탭을 선택했을 때 출력해주세요. */}
+      {/* <ListRow contents={<ListRow.Texts type="1RowTypeA" top="상품을 선택해주세요." />} /> */}
+    </>
+  );
+}
+
+export default CalcResult;
---
+++ src/widgets/Products.tsx
@@ -0,0 +1,13 @@
+import FilteredList from '../components/FilteredList.tsx';
+import { Spacing } from 'tosslib';
+
+function Products() {
+  return (
+    <>
+      <FilteredList />
+      <Spacing size={8} />
+    </>
+  );
+}
+
+export default Products;
